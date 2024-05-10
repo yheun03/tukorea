1c05b979aa0067bcadda3aed829b8aea92c38bf8
Younghwan <eyh208@gmail.com>
2024-05-10 13:28:25 +0900

    식자재 등록 추가 완료

--- resources/scripts/scripts.js
+++ resources/scripts/scripts.js
@@ -1,8 +1,13 @@
 $(document).ready(function(){
+    // NOTE: 홈 화면 광고배너
     $('.banner ul').slick({
         arrows: false,
         centerMode: true,
         centerPadding: '12px',
     })
     
+    // NOTE: 레시피 등록 화면 추가 설명 Toggle
+    $(".recipeInsertSteps .btn-wrap label").click(function(){
+        $(this).toggleClass("active")
+    })
 })
